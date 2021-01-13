539e04ed2ed3e47029630aaad6e106ba02b6bcaf
Josh Leong <i18njosh@mgail.com>
2021-01-13 08:07:02 -0700

    removed gh icon

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { GitHubIcon} from '../components/icons'
+// import { GitHubIcon} from '../components/icons'
 
 const About = () => {
     
